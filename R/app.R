63c0096f051c029d554163ed9c333394c5b168a2
Simon Garnier <garnier@njit.edu>
2020-10-22 08:19:24 +0200

    Can't change folder name. This is stupid

--- R/app.R
+++ R/app.R
@@ -1,6 +1,6 @@
 #' @export
 brio <- function(...) {
-  app_path <- paste0(find.package("bRio"), "/app/brio")
+  app_path <- paste0(find.package("bRio"), "/app/bRio")
 
   shiny::runApp(app_path, ...)
 }
