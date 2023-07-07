72d3d62a4e24b1e83f4e5deed638d919306549ed
Simon Garnier <garnier@njit.edu>
2023-07-07 12:15:14 +0200

    Update global.R

--- inst/app/bRio/global.R
+++ inst/app/bRio/global.R
@@ -6,40 +6,39 @@ library(shinyFiles)
 library(Rvision)
 
 
-#### Globals #####
-if (Sys.info()["sysname"] == "Linux") {
-  dev <- suppressWarnings(system("ls /dev/video*", intern = TRUE,
-                ignore.stderr = TRUE))
-
-  if (length(dev > 0)) {
+#### Custom functions ####
+listCams <- function() {
+  if (Sys.info()["sysname"] == "Linux") {
+    dev <- system2("ls", "/dev/video*", TRUE)
     dev_id <- as.numeric(gsub("/dev/video", "", dev))
-    cams <- list()
+  } else if (Sys.info()["sysname"] == "Darwin") {
+    dev <- system2("system_profiler", "SPCameraDataType", TRUE)
+    dev_id <- 0:(sum(grepl("Unique ID", dev)) - 1)
+  } else {
+    list()
+  }
 
-    cams <- lapply(dev_id, function(i) {
-      st <- tryCatch(stream(i, api = "V4L2"), error = function(e) FALSE)
+  cams <- lapply(dev_id, function(i) {
+    st <- tryCatch(stream(i, api = "V4L2"), error = function(e) FALSE)
 
-      if (isStream(st)) {
-        setProp(st, "FOURCC", fourcc("MJPG"))
-        setProp(st, "FRAME_WIDTH", 4096)
-        setProp(st, "FRAME_HEIGHT", 2160)
+    if (isStream(st)) {
+      setProp(st, "FOURCC", fourcc("MJPG"))
+      setProp(st, "FRAME_WIDTH", 4096)
+      setProp(st, "FRAME_HEIGHT", 2160)
 
-        if (getProp(st, "FRAME_WIDTH") == 4096 & getProp(st, "FRAME_HEIGHT") == 2160) {
-          st
-        } else {
-          release(st)
-          NULL
-        }
+      if (getProp(st, "FRAME_WIDTH") == 4096 & getProp(st, "FRAME_HEIGHT") == 2160) {
+        st
       } else {
+        release(st)
         NULL
       }
-    })
+    } else {
+      NULL
+    }
+  })
 
-    cams <- cams[lengths(cams) != 0]
-  } else {
-    cams <- list()
-  }
-} else {
-  stop("Unsupported platform.")
+  cams <- cams[lengths(cams) != 0]
+  cams
 }
 
 zoom <- function(img, f) {
@@ -55,4 +54,6 @@ zoom <- function(img, f) {
 }
 
 
+#### Globals #####
+cams <- listCams()
 volumes <- getVolumes()
