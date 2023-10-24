d5184d78384d2e82b3e91d0845664262458ac33f
Simon Garnier <garnier@njit.edu>
2023-10-24 15:19:46 -0400

    Add documentation.

--- R/app.R
+++ R/app.R
@@ -1,3 +1,13 @@
+#' @title Brio Application
+#'
+#' @description \code{brio} launches the `brio` shiny application.
+#'
+#' @param ... Optional parameters to be passed to \code{\link[shiny]{runApp}}.
+#'
+#' @return This function launches the app and returns nothing.
+#'
+#' @author Simon Garnier, \email{garnier@@njit.edu}
+#'
 #' @export
 brio <- function(...) {
   app_path <- paste0(find.package("bRio"), "/app/bRio")
